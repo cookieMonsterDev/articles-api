2be37565d426e457115b46489db948ced7f2083e
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2022-11-13 17:47:45 +0200

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {console.log('hello world')})
+
+app.get('/', (req, res) => {
+  res.send({data: 'hello'})
+})
